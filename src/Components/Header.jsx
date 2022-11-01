53462d35bc35e5d8a9f6bbb4e3559863f0846f4f
mariunsj <mariunsj@stud.ntnu.no>
2022-11-01 10:45:36 +0100

    Added logo and changed theme

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css"
 import { ProfileAdd, Messages3 } from "iconsax-react";
+import Logo from "./Logo";
 
 const Header = () => {
   return (
     <div className="header">
-      <Link to="/" className="pageName">BetterTech</Link>
+      <Link to="/" className="pageName">
+        <Logo width="200" height="100" />
+      </Link>
       <Link to="/" className="logoWithText">
         <Messages3 />
         <h3>Contact</h3>
