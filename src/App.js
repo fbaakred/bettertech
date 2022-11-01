1fdbee20ef26efea2a0b25ba2283fc641566ce41
mariunsj <mariunsj@stud.ntnu.no>
2022-11-01 18:33:31 +0100

    Added simple navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import Homepage from "./Pages/Homepage";
 import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <Link to="/" className="navbaritem"> Laptops </Link>
+        <Link to="/" className="navbaritem"> Smartphones </Link>
+        <Link to="/" className="navbaritem"> Desktop computers </Link>
+        <input />
+    </div>
+  );
+}
+
+export default Navbar;
