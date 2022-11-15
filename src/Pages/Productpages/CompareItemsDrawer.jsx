e7eca505eb8a2bdb480b0f56983352a5c3df29f2
NicolasHa00 <nicolas.hautschek@googlemail.com>
2022-11-15 11:53:49 +0100

    first visual changes for comparison

--- src/Pages/Productpages/CompareItemsDrawer.jsx
+++ src/Pages/Productpages/CompareItemsDrawer.jsx
@@ -5,7 +5,7 @@ import itemsToCompare from "../../Recoil/Atoms/itemsToCompare";
 import { Link } from 'react-router-dom';
 import { storage } from "../../firebase";
 import { getBackgroundColor } from "../../utils";
-import { ArrowSwapHorizontal } from "iconsax-react";
+import { ArrowSwapHorizontal, Trash } from "iconsax-react";
 
 const DrawerItem = (props) => {
 
@@ -16,21 +16,21 @@ const DrawerItem = (props) => {
 
   useEffect(() => {
     getDownloadURL(ref(storage, product.id + '.jpg')).then((url) => {
-        setImgUrl(url);
+      setImgUrl(url);
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [compareItems]);
 
-  const removeItem = () => {    
-    const items = compareItems.filter(item => item.name !== product.name); 
+  const removeItem = () => {
+    const items = compareItems.filter(item => item.name !== product.name);
     setCompareItems(items);
   }
 
   return (
-    <div className="drawerItem" style={{backgroundColor: getBackgroundColor(product.co2score)}}>
-      <img src={imgURL} alt="productphoto" height={"100px"} width={"140px"}/>
+    <div className="drawerItem" style={{ borderColor: getBackgroundColor(product.co2score) }}>
+      <button className="drawerItemButton" onClick={removeItem}> <Trash /> </button>
+      <img src={imgURL} alt="productphoto" height={"100px"} width={"140px"} />
       <div> {product.name} </div>
-      <button className="drawerItemButton" onClick={removeItem}> Remove </button>
     </div>
   );
 }
@@ -38,29 +38,32 @@ const DrawerItem = (props) => {
 const CompareItemsDrawer = () => {
   const [selectedItems, setSelectedItems] = useRecoilState(itemsToCompare);
   return (
-    selectedItems.length === 0 ? 
-      <></> 
+    selectedItems.length === 0 ?
+      <></>
       :
       <div>
-        <div style={{height: "120px"}}></div>
+        <div style={{ height: "135px" }}></div>
         <div className="drawerOpen">
           <div className='drawerContent'>
             {selectedItems.map((item, key) => (
-              <DrawerItem key={key} item={item}/>
+              <DrawerItem key={key} item={item} />
             ))}
           </div>
           <div className='drawerButtons'>
-            <button className="removeChosenButton" onClick={() => setSelectedItems([])}> Remove all chosen items </button>
-            <Link to="/compare" className="navbaritem"> 
-              <button className="compareChosenButton"> 
+            <button className="removeChosenButton" onClick={() => setSelectedItems([])}>
+              <Trash />
+              <div>Remove All Items</div>
+            </button>
+            <Link to="/compare" className="navbaritem">
+              <button className="compareChosenButton">
                 <ArrowSwapHorizontal />
-                <div>Compare chosen items</div>
+                <div>Compare Items</div>
               </button>
             </Link>
           </div>
         </div>
       </div>
-    
+
   );
 }
 export default CompareItemsDrawer;
